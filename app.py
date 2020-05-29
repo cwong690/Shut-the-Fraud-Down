fc1a938cc9ebd727918508c8b0e83d19af30c23d
Tyler Woods <tyjordan.woods@gmail.com>
2020-05-28 21:53:24 -0700

    finish day 1

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify 
+import pickle
+import numpy as np
+
+# Create app
+app = Flask(__name__)
+
+# Render home page
+@app.route('/',methods=['GET'])
+def home():
+
+    return render_template('home.html')
+
+    # return ''' <p> nothing here, friend but a link to
+    #                <a href="/hello">hello</a> and an 
+    #                <a href="/form_example">example form</a> </p> '''
+
+# @app.route('/hello', methods=['GET'])
+# def hello_word():
+#     return ''' <h1> Hello, World!</h1> '''
+
+# @app.route('/form_example', methods=['GET'])
+# def form_display():
+#     return ''' <form action="/string_reverse" method="POST">
+#                 <input type="text" name="some_string" />
+#                 <input type="submit" />
+#                </form>
+#              '''
+
+# @app.route('/string_reverse', methods=['POST'])
+# def reverse_string():
+#     text=str(request.form['some_string'])
+#     reversed_string = text[-1::-1]
+#     return ''' output: {}   '''.format(reversed_string)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
